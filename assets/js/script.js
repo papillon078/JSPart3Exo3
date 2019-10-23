e5ceaa3ca44d4b80d2441ab5ff8c5f0e7545fda9
papillon078 <ldc.morin@gmail.com>
2019-10-23 15:27:04 +0200

    exo 3 avant correction

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function greenLight(){
+document.getElementById('text').style.color = 'green';
+}
+function redLight(){
+document.getElementById('text').style.color = 'red';
+}
+function blueLight(){
+document.getElementById('text').style.color = 'blue';
+}
